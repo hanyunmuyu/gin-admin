9b3634d7226703391ccd87e38b3aa17d83eb3fdc
hanyun <1355081829@qq.com>
2021-01-05 17:47:22 +0800

    用户列表

--- app/http/controllers/admins/user_controller.go
+++ app/http/controllers/admins/user_controller.go
@@ -15,6 +15,16 @@ type UserController struct {
 	http.BaseController
 }
 
+// @Summary 用户列表
+// @Security ApiKeyAuth
+// @Description | 参数 | 说明 |备注|
+// @Description | :-----: | :----: | :----: |
+// @Description |page|页码||
+// @Tags  admin
+// @version 1.0
+// @Param page query int false "页码" default(1)
+// @success 200 {object} utils.JSONResult{data=[]models.User} "desc"
+// @Router /admin/v1/user/list [GET]
 func (userController *UserController) GetUserList(ctx *gin.Context) {
 	page := 1
 	if p, err := strconv.Atoi(ctx.DefaultQuery("page", "1")); err != nil {
