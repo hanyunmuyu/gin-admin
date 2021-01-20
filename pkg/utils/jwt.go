d335d63983b5a78e49417d7b40e785c8346ac00a
hanyun <1355081829@qq.com>
2021-01-20 11:51:26 +0800

    监听配置文件改变

--- pkg/utils/jwt.go
+++ pkg/utils/jwt.go
@@ -5,18 +5,30 @@ import (
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"strconv"
+	"time"
 )
+
+func CreateToken(key string, data interface{}) (string, error) {
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		key:   data,
+		"exp": time.Now().Add(time.Hour * time.Duration(24*Config().GetInt("jwt.expiresAt"))).Unix(),
+	})
+	token, err := at.SignedString([]byte(Config().GetString("jwt.signingKey")))
+	if err != nil {
+		return "", err
+	}
+	return token, nil
+}
 
 func ParseToken(ctx *gin.Context) (uint, error) {
 	authorization := ctx.GetHeader("Authorization")
 	claim, err := jwt.Parse(authorization, func(token *jwt.Token) (interface{}, error) {
-		return []byte("123456"), nil
+		return []byte(Config().GetString("jwt.signingKey")), nil
 	})
 	if err != nil {
 		return 0, err
 	}
 	if admin, ok := claim.Claims.(jwt.MapClaims); ok {
-
 		adminMap, _ := admin["admin"].(map[string]interface{})
 		id, _ := adminMap["id"]
 		adminId := 0
